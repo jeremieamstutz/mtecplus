00cc719c10c8d7fa1a1a7afbe36e51e5c57edc19
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2023-01-08 20:14:13 +0100

    create input

---
+++ components/ui/input/input.js
@@ -0,0 +1,14 @@
+import cn from 'clsx'
+
+import s from './input.module.css'
+
+export function Input({ name, label, ...props }) {
+	return (
+		<div className={s.group}>
+			<label className={s.label} htmlFor={name}>
+				{label}
+			</label>
+			<input className={cn(s.input, props.className)} {...props} />
+		</div>
+	)
+}
