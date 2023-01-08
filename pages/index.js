8be1431abb1451ff8288d9bfd80dd56ccf84f0e9
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2023-01-08 20:20:01 +0100

    add sponsors

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,59 @@
-import { Hero } from 'components/elements'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Marquee from 'react-fast-marquee'
+
+import { Hero } from 'components/elements'
+
+import s from 'styles/pages/home.module.css'
+
+const sponsors = [
+	{
+		name: 'KPMG',
+		logo: 'https://logo.clearbit.com/home.kpmg?s=256&greyscale="true',
+		website: 'https://home.kpmg',
+	},
+	{
+		name: 'PwC',
+		logo: 'https://logo.clearbit.com/pwc.ch?s=256&greyscale="true',
+		website: 'https://pwc.ch',
+	},
+	{
+		name: 'ETHZ',
+		logo: 'https://logo.clearbit.com/ethz.ch?s=256&greyscale="true',
+		website: 'https://ethz.ch',
+	},
+	{
+		name: 'KPMG',
+		logo: 'https://logo.clearbit.com/home.kpmg?s=256&greyscale="true',
+		website: 'https://home.kpmg',
+	},
+	{
+		name: 'PwC',
+		logo: 'https://logo.clearbit.com/pwc.ch?s=256&greyscale="true',
+		website: 'https://pwc.ch',
+	},
+	{
+		name: 'ETHZ',
+		logo: 'https://logo.clearbit.com/ethz.ch?s=256&greyscale="true',
+		website: 'https://ethz.ch',
+	},
+	{
+		name: 'KPMG',
+		logo: 'https://logo.clearbit.com/home.kpmg?s=256&greyscale="true',
+		website: 'https://home.kpmg',
+	},
+	{
+		name: 'PwC',
+		logo: 'https://logo.clearbit.com/pwc.ch?s=256&greyscale="true',
+		website: 'https://pwc.ch',
+	},
+	{
+		name: 'ETHZ',
+		logo: 'https://logo.clearbit.com/ethz.ch?s=256&greyscale="true',
+		website: 'https://ethz.ch',
+	},
+]
 
 export default function Home() {
 	return (
@@ -10,6 +64,26 @@ export default function Home() {
 				</style>
 			</Head>
 			<Hero title="MTEC+" />
+			<h2>Sponsors</h2>
+			<p>We are grateful for the generous support of our sponsors.</p>
+			<Marquee className={s.marquee} speed={20} gradient={false}>
+				{sponsors.map(({ name, logo, website }, idx) => (
+					<Link
+						href={website}
+						key={idx}
+						style={{ textDecoration: 'none' }}
+					>
+						<Image
+							className={s.logo}
+							src={logo}
+							alt={name}
+							width={80}
+							height={80}
+							priority={true}
+						/>
+					</Link>
+				))}
+			</Marquee>
 		</>
 	)
 }
