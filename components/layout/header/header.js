8f8b529e4062c988cb8a203d3ef966c3c2aca2a7
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2023-01-08 20:19:21 +0100

    add responsiveness

--- components/layout/header/header.js
+++ components/layout/header/header.js
@@ -1,68 +1,129 @@
 import cn from 'clsx'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useEffect, useRef, useState } from 'react'
+
+import useClickOutside from 'lib/hooks/useClickOutside'
 
 import s from './header.module.css'
 
 export function Header() {
 	const router = useRouter()
+	const [open, setOpen] = useState(false)
+	const headerRef = useRef()
+
+	useClickOutside(headerRef, () => setOpen(false))
+
+	useEffect(() => {
+		setOpen(false)
+	}, [router.asPath])
 
 	return (
-		<header className={s.header}>
-			<Link href="/" className={s.logo}>
-				MTEC+
-			</Link>
-			<nav className={s.nav}>
-				<Link
-					href="/"
-					className={cn(s.link, { [s.active]: router.asPath == '/' })}
-				>
-					Home
+		<header className={s.header} ref={headerRef}>
+			<div className={s.main}>
+				<Link href="/" className={s.logo}>
+					MTEC<span className={s.plus}>+</span>
 				</Link>
-				<Link
-					href="/about"
-					className={cn(s.link, {
-						[s.active]: router.asPath == '/about',
-					})}
+				<button
+					className={s.button}
+					onClick={() => setOpen((prev) => !prev)}
 				>
-					About
+					{open ? (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							width={24}
+							height={24}
+						>
+							<path
+								strokeLinecap="square"
+								strokeLinejoin="square"
+								d="M6 18L18 6M6 6l12 12"
+							/>
+						</svg>
+					) : (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							width={24}
+							height={24}
+						>
+							<path
+								strokeLinecap="square"
+								strokeLinejoin="square"
+								d="M3.75 9h16.5m-16.5 6.75h16.5"
+							/>
+						</svg>
+					)}
+				</button>
+			</div>
+			<div className={cn(s.menu, { [s.open]: open })}>
+				<nav className={s.nav}>
+					<Link
+						href="/"
+						className={cn(s.link, {
+							[s.active]: router.pathname == '/',
+						})}
+					>
+						Home
+					</Link>
+					<Link
+						href="/about"
+						className={cn(s.link, {
+							[s.active]: router.pathname == '/about',
+						})}
+					>
+						About
+					</Link>
+					<Link
+						href="/events"
+						className={cn(s.link, {
+							[s.active]: router.pathname.startsWith('/events'),
+						})}
+					>
+						Events
+					</Link>
+					<Link
+						href="/services"
+						className={cn(s.link, {
+							[s.active]: router.pathname == '/services',
+						})}
+					>
+						Services
+					</Link>
+					<Link
+						href="/info"
+						className={cn(s.link, {
+							[s.active]: router.pathname == '/info',
+						})}
+					>
+						Info
+					</Link>
+					<Link
+						href="/jobs"
+						className={cn(s.link, {
+							[s.active]: router.pathname.startsWith('/jobs'),
+						})}
+					>
+						Jobs
+					</Link>
+					<Link
+						href="/students"
+						className={cn(s.link, {
+							[s.active]: router.pathname.startsWith('/students'),
+						})}
+					>
+						Students
+					</Link>
+				</nav>
+				<Link href="/login" className={s.login}>
+					Login
 				</Link>
-				<Link
-					href="/events"
-					className={cn(s.link, {
-						[s.active]: router.asPath.startsWith('/events'),
-					})}
-				>
-					Events
-				</Link>
-				<Link
-					href="/services"
-					className={cn(s.link, {
-						[s.active]: router.asPath == '/services',
-					})}
-				>
-					Services
-				</Link>
-				<Link
-					href="/info"
-					className={cn(s.link, {
-						[s.active]: router.asPath == '/info',
-					})}
-				>
-					Info
-				</Link>
-				<Link
-					href="/students"
-					className={cn(s.link, {
-						[s.active]: router.asPath == '/students',
-					})}
-				>
-					Students
-				</Link>
-			</nav>
-			<Link href="/login" className={s.login}>
-				Login
-			</Link>
+			</div>
 		</header>
 	)
 }
