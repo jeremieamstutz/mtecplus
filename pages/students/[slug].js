b32cfac7d750fd0c3d639210ca03cbb3acae6844
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2023-01-08 20:24:00 +0100

    add students pages

---
+++ pages/students/[slug].js
@@ -0,0 +1,71 @@
+import { students } from 'lib/constants/students'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import s from 'styles/pages/student.module.css'
+
+export default function Student() {
+	const router = useRouter()
+
+	const student = students.filter(
+		(student) => student.slug === router.query.slug,
+	)[0]
+
+	if (!student) return
+
+	const {
+		first_name,
+		last_name,
+		roles,
+		education,
+		picture,
+		contact: { email, linkedin, instagram },
+	} = student
+
+	return (
+		<>
+			<Head>
+				<style>
+					{':root { --background-color: #ecd7d1 !important; }'}
+				</style>
+			</Head>
+			<section className={s.header}>
+				<Image
+					className={s.picture}
+					src={picture}
+					alt={name}
+					width={256}
+					height={256}
+				/>
+				<div className={s.info}>
+					<h1 className={s.name}>{`${first_name} ${last_name}`}</h1>
+					<div className={s.roles}>
+						{roles.map(({ name }) => (
+							<div className={s.role}>{name}</div>
+						))}
+					</div>
+					<div className={s.education}>{education}</div>
+					<div className={s.links}>
+						{email && (
+							<Link className={s.link} href={email}>
+								Linkedin
+							</Link>
+						)}
+						{linkedin && (
+							<Link className={s.link} href={linkedin}>
+								Linkedin
+							</Link>
+						)}
+						{instagram && (
+							<Link className={s.link} href={instagram}>
+								Instagram
+							</Link>
+						)}
+					</div>
+				</div>
+			</section>
+		</>
+	)
+}
---
+++ pages/students/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+
+import { students } from 'lib/constants/students'
+
+import { Hero, Member } from 'components/elements'
+
+import s from 'styles/pages/students.module.css'
+import { Input } from 'components/ui'
+import { useState } from 'react'
+
+export default function Students() {
+	const [query, setQuery] = useState('')
+
+	const filteredStudents = students.filter(
+		({ first_name, last_name }) =>
+			(first_name + ' ' + last_name)
+				.toLowerCase()
+				.indexOf(query.toLowerCase()) > -1,
+	)
+
+	return (
+		<>
+			<Head>
+				<style>
+					{':root { --background-color: #ecd7d1 !important; }'}
+				</style>
+			</Head>
+			<Hero title="Student Directory" />
+			<Input
+				type="search"
+				placeholder="Search..."
+				value={query}
+				onChange={(event) => setQuery(event.target.value)}
+			/>
+			<div className={s.students}>
+				<h2 className={s.year}>Class of 2022</h2>
+				<div className={s.class}>
+					{filteredStudents.map((student, idx) => (
+						<Member member={student} key={idx} />
+					))}
+				</div>
+			</div>
+		</>
+	)
+}
