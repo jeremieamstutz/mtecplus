f1bb16692fe06fa9df3c9a455080be9a9850a975
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2023-01-08 20:19:50 +0100

    fill login page

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,25 @@
 import { Hero } from 'components/elements'
+import { Button, Input } from 'components/ui'
+
+import s from 'styles/pages/login.module.css'
 
 export default function Login() {
-	return <Hero title="Login" />
+	return (
+		<>
+			<Hero title="Login" subtitle="Please use your ETH email address" />
+			<div
+				style={{
+					width: '100%',
+					maxWidth: '480px',
+					display: 'flex',
+					flexDirection: 'column',
+					gap: '1rem',
+				}}
+			>
+				<Input name="email" label="Email" type="email" />
+				<Input name="password" label="Password" type="password" />
+				<Button className={s.button}>Login</Button>
+			</div>
+		</>
+	)
 }
