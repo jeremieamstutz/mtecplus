b6f489e7fd6c9d9a3c84c5de0f2aaa3129d637e5
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2023-01-09 02:48:21 +0100

    add link button

--- components/ui/button/button.js
+++ components/ui/button/button.js
@@ -1,7 +1,15 @@
 import cn from 'clsx'
+import Link from 'next/link'
 
 import s from './button.module.css'
 
-export function Button({ children, ...props }) {
+export function Button({ children, href, ...props }) {
+	if (href) {
+		return (
+			<Link className={cn(s.button, props.className)} href={href}>
+				{children}
+			</Link>
+		)
+	}
 	return <button className={cn(s.button, props.className)}>{children}</button>
 }
