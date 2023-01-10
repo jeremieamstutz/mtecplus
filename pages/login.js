68177887c307d01dcb892a41e7be33c04cc6dfdb
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2023-01-11 00:51:21 +0100

    move to formik

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import { Hero } from 'components/elements'
 import { Meta } from 'components/layout'
 import { Button, Input } from 'components/ui'
+import { Form, Formik } from 'formik'
 
 import s from 'styles/pages/login.module.css'
 
@@ -10,19 +11,23 @@ export default function Login() {
 			<Meta title="Login" />
 			{/* <Hero title="Login" subtitle="Please use your ETH email address" /> */}
 			<Hero title="Login" subtitle="(Doesn't work at the moment)" />
-			<div
-				style={{
-					width: '100%',
-					maxWidth: '480px',
-					display: 'flex',
-					flexDirection: 'column',
-					gap: '1rem',
-				}}
-			>
-				<Input name="email" label="Email" type="email" />
-				<Input name="password" label="Password" type="password" />
-				<Button className={s.button}>Login</Button>
-			</div>
+			<Formik>
+				<Form
+					style={{
+						width: '100%',
+						maxWidth: '480px',
+						display: 'flex',
+						flexDirection: 'column',
+						gap: '1rem',
+					}}
+				>
+					<Input name="email" label="Email" type="email" />
+					<Input name="password" label="Password" type="password" />
+					<Button type="submit" className={s.button}>
+						Login
+					</Button>
+				</Form>
+			</Formik>
 		</>
 	)
 }
